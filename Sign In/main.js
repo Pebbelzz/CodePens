9c5de596faffaeefe64dc29bb124794d1659de08
Pebbelzz <greeneyes14j@gmail.com>
2016-01-24 17:51:19 -0500

    Added Sign In

---
+++ Sign In/main.js
@@ -0,0 +1,7 @@
+function buttonClicked(){
+	document.getElementById("submit").style.cssText = 'box-shadow: 0 0 0 0;';
+}
+
+function buttonReturn(){
+	document.getElementById("submit").style.cssText = 'box-shadow: 0 5px 2px -2px #2EB56C;';
+}
